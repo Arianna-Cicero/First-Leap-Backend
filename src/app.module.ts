bdcbed341e0897f833b166456f18cfbfc5555b27
joaoaraujo2003 <joaopda15@gmail.com>
2024-04-28 23:47:02 +0100

    Updating files

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,22 @@ import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import { APP_FILTER } from '@nestjs/core';
 import { GlobalExceptionFilter } from './exceptions/global-exception.filter';
+import { EmailverificationModule } from './resources/emailverification/emailverification.module';
 import { DatabaseService } from './services/database.service';
-
+import { HealthController } from './controllers/health.controller';
+import { PostalCodeModule } from './resources/postal_code/postal_code.module';
+import { JobOfferModule } from './resources/job_offer/job_offer.module';
+import { JobtypeModule } from './resources/jobtype/jobtype.module';
+import { UtilizadorModule } from './resources/utilizador/utilizador.module';
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule],
-  controllers: [AppController],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule,
+    // EmailverificationModule,
+    PostalCodeModule,
+    JobOfferModule,
+    JobtypeModule,
+    // UtilizadorModule,
+  ],
+  controllers: [AppController,  HealthController],
   providers: [
     { provide: APP_FILTER, useClass: GlobalExceptionFilter },
     AppService,
