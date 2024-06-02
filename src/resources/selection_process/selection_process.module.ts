8582c989962e2c1e9c1902835691332f14862c7e
Arianna Cicero <arianna.cicero.c@gmail.com>
2024-06-02 15:19:32 +0100

    update: correct error of dependencies

--- src/resources/selection_process/selection_process.module.ts
+++ src/resources/selection_process/selection_process.module.ts
@@ -1,11 +1,10 @@
 import { forwardRef, Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { SelectionProcessController } from './selection_process.controller';
 import { SelectionProcessService } from './selection_process.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { SelectionProcess } from './entities/selection_process.entity';
-import { CandidateService } from '../candidate/candidate.service';
+import { CandidateModule } from '../candidate/candidate.module'; // Ensure CandidateModule is imported
 import { SelectionPhaseService } from '../selection_phase/selection_phase.service';
-import { CandidateModule } from '../candidate/candidate.module';
 import { EmailService } from 'src/mailer/sendMail';
 import { JobOfferService } from '../job_offer/job_offer.service';
 import { JobOfferModule } from '../job_offer/job_offer.module';
@@ -21,7 +20,6 @@ import { SchedulerModule } from 'src/modules/scheduler.module';
   controllers: [SelectionProcessController],
   providers: [
     SelectionProcessService,
-    CandidateService,
     SelectionPhaseService,
     EmailService,
     JobOfferService,
