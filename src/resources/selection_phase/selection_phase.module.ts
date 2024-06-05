aa9053ae01efd36f9abf09c299799b0a2ba09337
Arianna Cicero <arianna.cicero.c@gmail.com>
2024-06-05 19:04:34 +0100

    update: selection process

--- src/resources/selection_phase/selection_phase.module.ts
+++ src/resources/selection_phase/selection_phase.module.ts
@@ -3,12 +3,17 @@ import { SelectionPhaseController } from './selection_phase.controller';
 import { SelectionPhaseService } from './selection_phase.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { SelectionPhase } from './entities/selection_phase.entity';
-import { SelectionProcess } from '../selection_process/entities/selection_process.entity';
+import { TypeSelectionProcess } from '../type_selection_process/entities/type_selection_process.entity';
+import { TypeSelectionProcessModule } from '../type_selection_process/type_selection_process.module';
+import { TypeSelectionProcessService } from '../type_selection_process/type_selection_process.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([SelectionPhase])],
+  imports: [
+    TypeOrmModule.forFeature([SelectionPhase, TypeSelectionProcess]),
+    forwardRef(() => TypeSelectionProcessModule),
+  ],
   controllers: [SelectionPhaseController],
-  providers: [SelectionPhaseService],
+  providers: [SelectionPhaseService, TypeSelectionProcessService],
   exports: [SelectionPhaseService, TypeOrmModule],
 })
 export class SelectionPhaseModule {}
--- src/resources/type_selection_process/type_selection_process.module.ts
+++ src/resources/type_selection_process/type_selection_process.module.ts
@@ -3,15 +3,15 @@ import { TypeSelectionProcessService } from './type_selection_process.service';
 import { TypeSelectionProcessController } from './type_selection_process.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeSelectionProcess } from './entities/type_selection_process.entity';
-import { SelectionProcessModule } from '../selection_process/selection_process.module';
+import { SelectionPhaseModule } from '../selection_phase/selection_phase.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([TypeSelectionProcess]),
-    forwardRef(() => SelectionProcessModule),
+    // forwardRef(() => SelectionPhaseModule),
   ],
   controllers: [TypeSelectionProcessController],
   providers: [TypeSelectionProcessService],
-  exports: [TypeSelectionProcessService],
+  exports: [TypeSelectionProcessService, TypeOrmModule],
 })
 export class TypeSelectionProcessModule {}
